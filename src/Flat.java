094d77121f90c662727d43b5d953349449a33b50
LukaszNaus <nauslukasz@gmail.com>
2022-09-21 19:15:04 +0200

    Create flat

---
+++ src/Flat.java
@@ -0,0 +1,12 @@
+public class Flat {
+    int floor;
+    String city;
+    boolean balcony;
+
+    Flat(int floor, String city, boolean balcony){
+        this.floor= floor;
+        this.city = city;
+        this.balcony = balcony;
+
+    }
+}
